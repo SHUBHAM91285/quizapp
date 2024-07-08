b1b93a02cb8b3a7c763a75101015868896e65b67
SHUBHAM NATH TIWARI <92241087+SHUBHAM91285@users.noreply.github.com>
2024-07-08 12:38:14 +0530

    Update settings.py

--- brainbunny/settings.py
+++ brainbunny/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-62h)55ns3s_ck#d@tn1*-dqq1+p54(-ii8s9+-jskq*+0dmhw!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['quizapp-bd0g.onrender.com', 'localhost']
 
 
 # Application definition
